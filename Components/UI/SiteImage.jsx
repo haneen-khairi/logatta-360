eb8cbe364e3b5419b73ae93d6f45e03344b28614
Haneen Khairi <77403713+haneen-khairi@users.noreply.github.com>
2023-10-22 21:28:21 +0300

    up

--- Components/UI/SiteImage.jsx
+++
@@ -1,9 +0,0 @@
-import { Image } from '@nextui-org/react'
-import React from 'react'
-
-export default function SiteImage({
-    src,
-    className = ""
-}) {
-  return <Image className={`${className} rounded-none`} removeWrapper={true} src={src}  />
-}
--- Functions/RegexFunction.js
+++
@@ -1,3 +0,0 @@
-export const requiredRegex = /[a-zA-Zء-ي0-9]/;
-export const emailRegex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
-export const passwordRegex = /^(?=.*?[A-Za-z])(?=.*?[a-zA-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/;
